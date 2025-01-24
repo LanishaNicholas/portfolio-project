47fb0f6c241c2fc2bd3843d59e1cff7843b437d8
Lanisha Nicholas <lanishanicholas@gmail.com>
2025-01-24 09:58:11 +0100

    Change the Url to live

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://portfolio-project-kiu9.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
